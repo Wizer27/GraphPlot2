dcabc987aa326db673735415fe484c93d2199c43
Wizer27 <svd2yar@gmail.com>
2025-05-01 10:25:33 +0300

    update

--- pages/Table.py
+++ pages/Table.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-data = pd.DataFrame(np.random.randn(20,3),columns =["Russia","Germany","USA"])
+data = pd.DataFrame(np.random.randn(20,3),columns = ["Russia","Germany","USA"] )
 st.area_chart(data)
