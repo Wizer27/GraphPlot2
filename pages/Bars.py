8863e15111835c369c5f4b4684ee635da267752f
Wizer27 <svd2yar@gmail.com>
2025-05-02 20:56:16 +0300

    update

--- pages/Bars.py
+++ pages/Bars.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 fig = plt.figure()
-plt.bar(['A', 'B', 'C'], [3, 7, 5])
+with st.sidebar:
+    val1 = st.text_input("Value1: ",placeholder = 'Enter the value1',value = 20)
+    val2 = st.text_input("Value2:  ",placeholder = 'Enter the value2',value = 10)
+    val3 = st.text_input("Value3: ",placeholder = 'Enter thr value3',value =5)
+    n1 =st.text_input("Name1:",placeholder = 'Enter the name1')
+    n2 = st.text_input("Name2: ",placeholder = 'Enter the name2')
+    n3 = st.text_input("Name3: ",placeholder = 'Enter the name3')
+plt.bar([n1,n2,n3], [float(val1), float(val2), float(val3)])
 st.pyplot(fig)
