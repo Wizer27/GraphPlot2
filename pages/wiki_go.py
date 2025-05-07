4adf37f87f5c41544162362d70069dd00ef5900e
Wizer27 <svd2yar@gmail.com>
2025-05-07 14:48:37 +0300

    update

--- pages/wiki_go.py
+++ pages/wiki_go.py
@@ -1,6 +1,9 @@
 import wikipedia as wk
 import streamlit as st
 #print(wk.summary("Java programming language"))
+st.markdown("""Wikipedia Search App
+This is a simple Streamlit application that allows users to search for any topic using the Wikipedia API.
+The app displays a summary of the requested topic or shows an error message if the topic is not found.""")
 task = st.text_input("Enter what you want to find")
 if task != '':
     try:  
