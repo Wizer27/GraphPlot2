0e96d287d4e4a29bdc8b910ac7d0eba49577713c
Wizer27 <svd2yar@gmail.com>
2025-05-07 12:14:42 +0300

    update

--- pages/Histogram.py
+++ pages/Histogram.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 data = np.random.rand(1000)
 fig = plt.figure()
 plt.hist(data,bins = 30)
-plt.title("Histogramm")
+with st.sidebar:
+    title = st.text_input("Enter the title")
+plt.title(title)
 st.pyplot(fig)
 ### New page
