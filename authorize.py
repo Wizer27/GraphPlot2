16b659b253ab021ae0d93669f534cfb5a29b3476
Wizer27 <svd2yar@gmail.com>
2025-05-08 18:23:49 +0300

    push

--- authorize.py
+++ authorize.py
@@ -9,4 +9,10 @@ def autor(username,password):
     if username in users and check_password_hash(users[username], password):
         return True
     return False   
-        
+def evens(iterable: iter) -> list:
+    even = []
+    for i in iter:
+        if i % 2 == 0:
+            even.append(i)
+    return even
+assert evens([1,2,3,4,5,6,7])      
