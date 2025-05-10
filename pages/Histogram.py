2610b00ba7094cf06e56330c7ce7c75998eba43d
Wizer27 <svd2yar@gmail.com>
2025-05-10 19:26:15 +0300

    update

--- pages/Histogram.py
+++ pages/Histogram.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
+from authorize import autor
+
+
+
+# ===== LOGIN PAGE ===== 
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+if not st.session_state.logged_in:
+    st.title("🔒 Вход в систему")
+    username = st.text_input("Логин")
+    password = st.text_input("Пароль", type="password")
+    if st.button("Войти"):
+        if autor(username, password):
+            st.session_state.logged_in = True
+            st.session_state.username = username
+            #st.experimental_rerun()  # Обновляем страницу
+        else:
+            st.error("Неверный логин или пароль")
+    st.stop()  # 🔒 Без входа — ничего не запускается дальше
+
+# 🟢 Если авторизован — запускаем интерфейс приложения
+st.success(f"✅ Привет, {st.session_state.username}!")     
+
 data = np.random.rand(1000)
 fig = plt.figure()
 plt.hist(data,bins = 30)
