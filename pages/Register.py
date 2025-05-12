8a8a7df4588737f0810ed20357c2bcf4dddce032
Wizer27 <svd2yar@gmail.com>
2025-05-12 12:12:54 +0300

    test add

--- pages/Register.py
+++ pages/Register.py
@@ -6,11 +6,12 @@ import json
 
 
 
-
-username = st.text_input('Username:',placeholder='Enter the username')
-pasword = st.text_input('Password',placeholder='Enter the password',type = 'password')
-pasword2 = st.text_input('Reenter',placeholder='Reenter the password',type = 'password')
-if pasword != pasword2:
-    st.error('Passwords doesnt math')
-else:
-    st.success('You created an account')    
+def main():
+    username = st.text_input('Username:',placeholder='Enter the username')
+    pasword = st.text_input('Password',placeholder='Enter the password',type = 'password')
+    pasword2 = st.text_input('Reenter',placeholder='Reenter the password',type = 'password')
+    if pasword != pasword2:
+        st.error('Passwords doesnt math')
+    else:
+        st.success('You created an account') 
+main()           
