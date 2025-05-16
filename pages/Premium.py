fadfeecaa396bf6af641a0be59f0e31d44d9466b
Wizer27 <svd2yar@gmail.com>
2025-05-16 10:42:31 +0300

    update add a stripe conf

--- pages/Premium.py
+++ pages/Premium.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import stripe
+from urllib.parse import parse_qs, urlparse
+
+stripe.api_key = 'i hate sasha mishin'
+def create_session(amount,currency='usd'):
+    try:
+        pass
+    except:
+        pass
 
 
 st.title('Buy premium subcription')
