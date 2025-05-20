e302c39ec41c69260f67ea30d0c42cca036e5716
Wizer27 <svd2yar@gmail.com>
2025-05-20 09:29:49 +0300

    first +- version of stripe configuration

--- pages/Premium.py
+++ pages/Premium.py
@@ -5,13 +5,19 @@ from urllib.parse import parse_qs, urlparse
 
 
 
-Public_key = 'public_key'
+#Public_key = 'public_key'
 stripe.api_key = 'i hate sasha mishin'
 def create_session(amount,currency='usd'):
     try:
-        pass
-    except:
-        pass
+        charge = stripe.Charge.create(
+            amount=amount,
+            currency=currency,
+            source=stripe.api_key,  # Тестовый токен (можно заменить на реальный)
+            description="Платеж через Python"
+        )
+        return charge
+    except stripe.error.StripeError as e:
+        st.error(f"Error {e}")
 
 
 st.title('Buy premium subcription')
